17e1d0ca6343930569c52720b7806a3024387119
Viktor <vite@mail.ru>
2020-02-03 17:19:40 +0300

    Изменил описание Интерфейса

---
+++ javaproject1/JavaRush/Exercises/Svetofor.java
@@ -0,0 +1,35 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class Svetofor {
+	
+	   public static void main(String[] args) {
+	    	
+	    	double t = new Scanner(System.in).nextDouble();
+	    	int a = (int) t % 60;
+
+	    	 for(int j = 0; j<=60; j = j + 5) {
+	    		 	
+	    		 if(a<j) {
+	    			 
+	    			 a = a - j + 5;
+	    			  
+	    			  if(a>=0 && a<3) {
+	    				  System.out.println("зеленый"); 
+	    				  break;
+	    			  	  }
+	    			  if(a==3) {
+	        			  System.out.println("желтый"); 
+	        			  break;
+	        			  }
+	    			  if(a==4) {
+	        			  System.out.println("красный"); 
+	        			  break;
+	        			  }	
+	    	 break;
+	    		}
+	    		  
+	    	}
+	   }
+}
