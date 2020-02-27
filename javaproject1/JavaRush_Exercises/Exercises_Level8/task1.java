78b303821c7be361e952beb68316051201554b0f
Viktor <vite@mail.ru>
2020-02-28 00:04:01 +0300

    Упростил задачу, убрав второй список

--- javaproject1/JavaRush_Exercises/Exercises_Level8/task1.java
+++ javaproject1/JavaRush_Exercises/Exercises_Level8/task1.java
@@ -27,12 +27,16 @@ public class task1 {
     		list.add(x);
     	}
     	
-    	int a = 1;
+    	int a = 1;    	
+    	int b = 1;
     	
     	for(int i = 0; i < list.size()-1; i++) {
 
     		if(list.get(i).equals(list.get(i+1))) {
     			a++;
+    			if(b < a) {
+        			b = a;	
+    			}
     		}
     		if(!list.get(i).equals(list.get(i+1))) {
     			list1.add(a);
@@ -41,18 +45,8 @@ public class task1 {
     		
     		if(i == list.size()-2) {
     			list1.add(a);
-    		}
-    		
+    		}	
     	}
-    	
-    	int max = list1.get(0);
-    	for(int i = 0; i < list1.size(); i++) {
-    			if(max < list1.get(i)) {
-    				max = list1.get(i);
-    			}
-    	}
-    	
-    	System.out.println(max);
-    	
+    	System.out.println(b);    	
     }
 }
