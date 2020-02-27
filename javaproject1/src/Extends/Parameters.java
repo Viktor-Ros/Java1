67a83faacf962151e0af85302eb86e12c5432b21
Viktor <vite@mail.ru>
2020-02-27 23:58:51 +0300

    Дополнил описание

--- javaproject1/src/Extends/Parameters.java
+++ javaproject1/src/Extends/Parameters.java
@@ -8,8 +8,9 @@ package Extends;
  * класс shevrolet -  наследник(extends) класса car
  * в классе car нет данных из класса shevrolet
  * цикличного наследования нет(car => chevrolet => nasledovanie =>car - нельзя)
+ * instanceof проверяет, является ли класс наследником другого класса, часто используется при переопределении equals, чтобы убедиться, что объект - строка
  *
-*/
+ */
 
 public class Parameters{
 
@@ -21,5 +22,7 @@ System.out.println("Скорость: " + chevrolet.speed);//дергаем пе
 System.out.println("Масса: " + chevrolet.weight());//дергаем метод из car
 System.out.println("Кузов: " + chevrolet.types());//дергаем метод из car
 System.out.println("Марка: " + chevrolet.model);//местная переменная из chevrolet
+System.out.print("chevrolet наследник Car: ");
+System.out.println(chevrolet instanceof Car);//instanceof проверяет, является ли chevrolet наследником Car
 }
 }
