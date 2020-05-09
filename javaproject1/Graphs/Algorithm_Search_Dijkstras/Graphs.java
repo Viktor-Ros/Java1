4d7320d559b0765a129bce88b506e1ca33d904d9
Viktor <vite@mail.ru>
2020-05-10 02:14:19 +0300

    Графы из интернета

--- javaproject1/Graphs/Algorithm_Search_Dijkstras/Graphs.java
+++ javaproject1/Graphs/Algorithm_Search_Dijkstras/Graphs.java
@@ -1,61 +1,89 @@
 package Algorithm_Search_Dijkstras;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 
-/*
- * 
- *		  ГРАФ
- * 		5------4
- * 	   / \	    \
- *    /   \      \
- *   0-----2------3
- * 	  \			  /
- *      \ 		/
- * 		  \	  /
- *          1
- * 
- *  0 => 1 = 20
- *  0 => 5 = 10
- *  1 => 3 = 30
- *  2 => 5 = 40
- *  2 => 0 = 20
- *  3 => 4 = 10
- *  3 => 2 = 20
- *  5 => 4 = 40
- *  
- */
 
 public class Graphs {
 		
-	public static void main(String[] args) {
-		
-		ArrayList<Integer>[] graph = new ArrayList[6];
+	public static void main(String[] args) throws IOException {
 
-		for(int i = 0; i < graph.length; i++) {
+        //URL url = new URL("http://www.algorithmsilluminated.org/datasets/problem9.8test.txt");
+        URL url = new URL("http://www.algorithmsilluminated.org/datasets/problem9.8.txt");
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));//поток чтения данных из url
+        
+        String b;//строка из url
+        
+        ArrayList<ArrayList<String>> str = new ArrayList<>();//список строк
+        
+        while((b = in.readLine()) != null) {//добавление строк в список без пробелов
+        	
+        	str.add(new ArrayList<String>());
+        	
+    		int count = 0;
+    		
+    		for(int i = 0; i < b.length(); i++) {
+    			
+    			
+    			if(b.charAt(i) == '	') {
+    				str.get(str.size() - 1).add(b.split("	")[count]);
+    				count++;
+    			}
+    			
+    			if(i == b.length() - 1 && b.charAt(i) != '	') {
+    				str.get(str.size() - 1).add(b.split("	")[count]);
+    			}
+    		}	
+        }
+
+        in.close();
+        
+        System.out.println(str);
+       
+		ArrayList<Integer>[] arr = new ArrayList[str.size() + 1];//матрица смежности
+		
+		for(int i = 0; i < arr.length; i++) {
 			
-			graph[i] = new ArrayList<Integer>();
+			arr[i] = new ArrayList<Integer>();
 			
-			for(int j = 0; j < graph.length; j++) {
+			for(int j = 0; j < arr.length; j++) {
 				
-				graph[i].add(1000);
+				arr[i].add(100000);
 			}
 		}
 		
-		graph[0].set(1,20);
-		graph[0].set(5,10);
-		graph[1].set(3,30);
-		graph[2].set(0,20);
-		graph[2].set(5,40);
-		graph[3].set(2,20);
-		graph[3].set(4,10);
-		graph[5].set(4,40);
+	       System.out.println("-----------------------------------------------------");
 		
-		for(int i = 0; i < graph.length; i++) {
+		for(int i = 0; i < str.size(); i++) {//заполнение матрица смежности
+						
+			ArrayList<String> a = str.get(i);
 			
-			System.out.println(graph[i]);	
+			int c = Integer.parseInt(a.get(0));
+	        
+	        for(int j = 1; j < a.size(); j++) {
+	        	
+	        	String d = a.get(j);
+	        	
+	        	int b1 = Integer.parseInt(d.split(",")[0]);//смежная вершина
+	        	int b2 = Integer.parseInt(d.split(",")[1]);//расстояние
+	        	
+	        	arr[c].set(b1, b2);
+	        }
+
 		}
 		
-		Dijkstras.Search(graph, 0);
+		for(int i = 0; i < arr.length; i++) {
 		
-	}
+	        System.out.println(arr[i]);
+		}
+
+	    System.out.println("-----------------------------------------------------");          
+        
+	    Dijkstras.Search(arr, 1);
+
+      }
 }
