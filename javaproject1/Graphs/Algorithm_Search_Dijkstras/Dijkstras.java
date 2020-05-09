c022eb32d9cae0c7cc9d098cec9d020b792d493a
Viktor <vite@mail.ru>
2020-05-10 02:16:33 +0300

    Добавил массив посещенный вершин для работы с неориентированными графами

--- javaproject1/Graphs/Algorithm_Search_Dijkstras/Dijkstras.java
+++ javaproject1/Graphs/Algorithm_Search_Dijkstras/Dijkstras.java
@@ -34,33 +34,39 @@ public class Dijkstras {
 				
 		int[] arrayMinLength = new int[graph.length];//массив с наименьшими путями
 		
-		Arrays.fill(arrayMinLength, 1000);//заполняем массив числами, большими чем любой путь графа(в теории это бесконечности)
+		boolean[] arrayVertex = new boolean[graph.length];//массив узлов, если узел посещен -  он true
+		
+		Arrays.fill(arrayMinLength, 10000);//заполняем массив числами, большими чем любой путь графа(в теории это бесконечности)
 		
 		arrayMinLength[vertexSearsh] = 0;//путь от и до главного узла
+		
+		arrayVertex[vertexSearsh] = true;
 		
 		System.out.println(Arrays.toString(arrayMinLength));
 		
 		ArrayDeque<Integer> deque = new ArrayDeque<Integer>();//очередь из узлов
 		
-		deque.push(vertexSearsh);//добавляем в очередь главный узел
+		deque.addFirst(vertexSearsh);//добавляем в очередь главный узел
 		
 		while(!deque.isEmpty()) {//пока очередь не пустая
 			
 			int vertex = deque.pop();//убираем из очереди верхний узел
 			
-			for(int i = 0; i < graph[vertex].size(); i++) {//перебираем узлы, смежные с верхним узлом учереди
+			for(int i = 1; i < graph[vertex].size(); i++) {//перебираем узлы, смежные с верхним узлом учереди
 				
-				if(arrayMinLength[vertex] + graph[vertex].get(i) < arrayMinLength[i]) {//если путь между узлами меньше текущего
+				if(arrayMinLength[vertex] + graph[vertex].get(i) < arrayMinLength[i] && arrayVertex[i] == false) {//если путь между узлами меньше текущего и текущий узел false
 					
 					arrayMinLength[i] = arrayMinLength[vertex] + graph[vertex].get(i);//устанавливаем новый путь
 					
 					deque.addLast(i);//добавляем смежный узел в конец очереди
+					
+					arrayVertex[i] = true;
 				}
 				
 			}
 			
+			System.out.println(deque);
 			System.out.println(Arrays.toString(arrayMinLength));
-		}
-		
+		}	
 	}
 }
