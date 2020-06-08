ebd41683432e31c96eb19409521cddfd30c408bf
Viktor <vite@mail.ru>
2020-06-08 20:57:34 +0300

    Обновил алгоритм Дейкстры

--- javaproject1/Graphs/Algorithm_Search_Dijkstras/Dijkstras.java
+++ javaproject1/Graphs/Algorithm_Search_Dijkstras/Dijkstras.java
@@ -1,9 +1,7 @@
 package Algorithm_Search_Dijkstras;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Stack;
 
 
 /*
@@ -22,67 +20,61 @@ import java.util.Stack;
  * После опять выбираем узел и ищем короткие пути
  * Повторяем пока не найдем все короткие пути
  * 
+ * поиск выполняется за O(n2+m), где n — кол-во вершин, m — кол-во рёбер
+ * 
  * 
  */
 
 
-
 public class Dijkstras {
 	
-	public static void Search(ArrayList<Integer>[] graph, int vertexSearsh) {
+	public static void Search(ArrayList<Integer>[] graph, int vertexSearch) {
 		
-		System.out.println("--------------------------------------");
-				
-	int[] arrayMinLength = new int[graph.length];//массив с длинами
-	
-	Arrays.fill(arrayMinLength, 1000000);//заполняем массив числами, большими чем любой путь графа(в теории это бесконечности)
-	
-	arrayMinLength[vertexSearsh] = 0;//путь от и до главного узла
-				
-	boolean[] arrayBool = new boolean[graph.length];//массив для помечания узлов
-						
-	int count = 0;//переменная для посчета операций
-			
-	for(int i = 1; i < arrayMinLength.length; i++) {
+		int[] arrayMinLength = new int[graph.length];//массив с наименьшими длинами
+		
+		Arrays.fill(arrayMinLength, Integer.MAX_VALUE);//заполняем массив числами, большими чем любой путь графа(в теории это бесконечности)
+		
+		arrayMinLength[vertexSearch] = 0;//путь от и до главного узла
 					
-        int vertex = 0; 
-        
-        int min = 1000000;
-             
-        for (int j = 1; j < arrayMinLength.length; j++) { 
-        	
-            if (arrayBool[j] == false && arrayMinLength[j] <= min) { 
-            	
-                min = arrayMinLength[j]; 
-                
-                vertex = j; 
-            }  
+		boolean[] arrayBool = new boolean[graph.length];//массив узла, если известно минимальное расстояние до узла -  он true
+											
+		for(int i = 1; i < arrayMinLength.length; i++) {//перебираем массив с наименьшими длинами
+						
+            int vertexMin = 0;//узел с минимальным расстоянием
             
-        count++;
-        
-        }
-		
-        arrayBool[vertex] = true;
-		
-        for (int k = 1; k < graph.length; k++) { 
-        	  
-            if (arrayBool[k] == false && arrayMinLength[vertex] + graph[vertex].get(k) < arrayMinLength[k]) {
+            int min = Integer.MAX_VALUE;//минимальное расстояние до узла 
+                 
+            for (int j = 1; j < arrayMinLength.length; j++) { //перебор для определения узла с минимальным расстоянием
             	
-            	arrayMinLength[k] = arrayMinLength[vertex] + graph[vertex].get(k); 
+                if (arrayBool[j] == false && arrayMinLength[j] <= min) {
+                	
+                    min = arrayMinLength[j];
+                    
+                    vertexMin = j; 
+                }  
             }
-        }	
-	}
-	
-	
-    System.out.println("Узел \t\t Минимальная дистанция"); 
-    
-    for (int i = 1; i < arrayMinLength.length; i++) {
-    	
-        System.out.println(i + " \t\t " + arrayMinLength[i]); 
-        
-    }
-
-	System.out.println("Кол-во операций = " + count);
+			
+            arrayBool[vertexMin] = true;
+			
+            for (int k = 1; k < graph.length; k++) {//перебор для определения минимального расстояния до узлов
+            	  
+                if (arrayBool[k] == false 
+                		
+                		&& arrayMinLength[vertexMin] + graph[vertexMin].get(k) < arrayMinLength[k]) {
+                	
+                	arrayMinLength[k] = arrayMinLength[vertexMin] + graph[vertexMin].get(k);//минимальное расстояние до узла k через узел minVertex
+                }
+            }	
+		}
 		
+        System.out.println("------------------------------------------------------------"); 
+		
+        System.out.println("Узел \t\t Минимальная дистанция"); 
+        
+        for (int i = 1; i < arrayMinLength.length; i++) {
+        	
+            System.out.println(i + " \t\t " + arrayMinLength[i]); 
+            
+        }	
 	}
 }
