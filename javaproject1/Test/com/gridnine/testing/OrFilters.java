1e90da83c063b1ce9edb49c1e872d00fa3757687
Viktor <vite@mail.ru>
2020-06-27 00:35:55 +0300

    Добавил описание класса

--- javaproject1/Test/com/gridnine/testing/OrFilters.java
+++ javaproject1/Test/com/gridnine/testing/OrFilters.java
@@ -3,31 +3,30 @@ package com.gridnine.testing;
 import java.util.Arrays;
 import java.util.List;
 
-public class OrFilters extends Filters{
+/*
+ * 
+ * Класс для фильтрации полетов по нескольким правилам по принципу ИЛИ
+ * Полет подходит фильтрацию, если подходит по любому условию
+ * 
+ */
 
-	
+public class OrFilters extends Filters{//фильтрация по ИЛИ
+
 	private List<Filters> filters;
 	
 	OrFilters(Filters ...filters){
-		
 		this.filters = Arrays.asList(filters);
 	}
 	
-	
 	@Override
 	boolean match(Flight flight) {
-		
 		boolean indicator = false;
-
 		for(int i = 0; i < filters.size(); i++) {
-			
-			indicator = filters.get(i).match(flight);
-				
-			if(indicator == true) {
+			indicator = filters.get(i).match(flight);	
+			if(indicator) {
 				break;
 			}
 		}
-		
 		return indicator;
 	}
 }
