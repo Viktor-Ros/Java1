6aeb5f93b3659ab91ae6c1dd4e9d0e1750fa0a88
Viktor <vite@mail.ru>
2020-06-26 20:52:20 +0300

    Класс для исключения полетов с сегментами, у которых дата прилета раньше даты вылета

---
+++ javaproject1/Test/com/gridnine/testing/SegmentArrBeforeDepFilter.java
@@ -0,0 +1,31 @@
+package com.gridnine.testing;
+
+import java.time.LocalDateTime;
+
+public class SegmentArrBeforeDepFilter extends Filters{//исключает полеты с сегментами у которых дата прилета раньше даты вылета
+
+	private boolean arrBeforeDep;
+
+	SegmentArrBeforeDepFilter(boolean arrBeforeDep){
+		
+		this.arrBeforeDep = arrBeforeDep;
+	}
+	
+	@Override
+	boolean match(Flight flight) {//исключает полеты с сегментами у которых дата прилета раньше даты вылета
+		
+		boolean indicator = true;
+		
+		for(int i = 0; i < flight.getSegments().size(); i++) {
+			
+			Segment segment = flight.getSegments().get(i);
+							
+			if(segment.getDepartureDate().isAfter((segment.getArrivalDate())) == true) {//сравнение даты прилета и даты вылета
+
+				indicator = false;
+				break;
+			}
+		}
+		return indicator;
+	}
+}
