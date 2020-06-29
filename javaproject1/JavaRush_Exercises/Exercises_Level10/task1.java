cb59ccb47304319209008ce8f2fdd75ea5c3d7c1
Viktor <vite@mail.ru>
2020-06-29 21:57:07 +0300

    Переписал код на работу с LikedHashMap

--- javaproject1/JavaRush_Exercises/Exercises_Level10/task1.java
+++ javaproject1/JavaRush_Exercises/Exercises_Level10/task1.java
@@ -1,39 +1,17 @@
 package Exercises_Level10;
 
-import java.io.BufferedReader;
-import java.io.CharConversionException;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.nio.file.FileSystemException;
-import java.rmi.RemoteException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Locale;
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
-import java.util.Set;
-
 
 /*
  * 
- * Ввести с клавиатуры 10 строчек
+ * Ввести с клавиатуры 10 строк
  * Подсчитать в них количество различных букв (для 33 маленьких букв алфавита). 
  * Результат вывести на экран в алфавитном порядке.
  * 
@@ -41,43 +19,34 @@ import java.util.Set;
 
 public class task1 {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-        // Алфавит
-        List<Character> alphabet = Arrays.asList(
-                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж',
-                'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
-                'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
-                'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
-
-        // Ввод строк
-        ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < 10; i++) {
-            String str = new Scanner(System.in).nextLine();
-            list.add(str);
-        }
-
-        ArrayList<Integer> list2 = new ArrayList<Integer>();//список слов
-        
-        for(int i = 0; i < alphabet.size(); i++ ) {
-        	
-        	int a = 0; 
-        	
-            for(int j = 0; j < list.size(); j++ ) {
-            	
-            	for(int k = 0; k < list.get(j).length(); k++ ) {
-            		if(list.get(j).charAt(k) == (alphabet.get(i))) {
-            			a++;
-            		}
-            	}
-            }
-            
-            list2.add(a);
-        }
-
-        for(int i = 0; i < alphabet.size(); i++ ) {
-        	
-        	System.out.println(alphabet.get(i) + " " + list2.get(i));
-        } 
-    }
+		List<Character> alp = Arrays.asList(
+	    	'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж',
+	        'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+	        'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+	        'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+	  
+		Map<Character, Integer> map = new LinkedHashMap<>();
+			
+		for(Character element : alp) {
+			map.put(element, 0);
+		}
+			
+	    for (int i = 0; i < 2; i++) {
+	    	String str = new Scanner(System.in).nextLine();
+	            
+	    	for(int j = 0; j < str.length(); j++) {
+	    		char b = str.charAt(j);			
+	    		map.replace(b, map.get(b) + 1);
+	    	}	
+	    }
+			
+	    Iterator<Map.Entry<Character, Integer>> iter = map.entrySet().iterator();
+	    	
+	    while(iter.hasNext()) {
+	    	Map.Entry<Character, Integer> it = iter.next();//Объединяет ключ и значения в один класс, чтобы не перебирать их по отдельности
+	    	System.out.println(it.getKey() + "  " + it.getValue());
+	    }
+	}
 }
