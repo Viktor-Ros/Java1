18c0475e47ce562844f3f3104319562bcb3abff8
Viktor <vite@mail.ru>
2020-06-29 22:22:49 +0300

    Сократил код за счет использования String и char

--- javaproject1/JavaRush_Exercises/Exercises_Level6/task1.java
+++ javaproject1/JavaRush_Exercises/Exercises_Level6/task1.java
@@ -1,46 +1,29 @@
 package Exercises_Level6;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 /*
  * 
- *Подсчет четных и нечетных цифр в числе 
- * 
+ * Подсчет четных и нечетных цифр в числе 
  * 
  */
 
 public class task1 {
 		   
-    public static int even;
-    public static int odd;
-
-    public static void main(String[] args) throws IOException {
-
-    	int a = new Scanner(System.in).nextInt();
-    	int b = 0;
-    	int c = a;
-    	int d;
-
-
-    	while(c!=0){//или String v = Integer.toString(a); b = v.length();
-    		c = c/10;
-        	b++;
-    	}
-
- 	  	
- 	  	
-    	for(int i = 0; i < b; i++) {
-    		d = (int) ((a%Math.pow(10,i+1) - a%Math.pow(10,i))/Math.pow(10,i));
-	
-        	if(d%2 == 0) {
-        		even++;
-        	}
-        	else odd++;
-
+    public static void main(String[] args){
+    	
+    	int even = 0, odd = 0;
+    	String a = new Scanner(System.in).nextLine();
+    	
+    	for(int i = 0; i < a.length(); i++) {
+    		
+    		char b = a.charAt(i);
+    		if(b%2 == 0) {
+            	even++;
+    		} else odd++;
     	}
     	
-    	System.out.println(even);
-    	System.out.println(odd);
+    	System.out.println("Четные: " + even);
+    	System.out.println("Нечетные: " + odd);
     }
 }
