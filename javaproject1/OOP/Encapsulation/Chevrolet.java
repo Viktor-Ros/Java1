750e034cf4458ff0e76f7670de9a5ad4cd32348d
Viktor <vite@mail.ru>
2020-06-30 17:22:05 +0300

    Добавил отступы в коде

--- javaproject1/OOP/Encapsulation/Chevrolet.java
+++ javaproject1/OOP/Encapsulation/Chevrolet.java
@@ -7,15 +7,15 @@ package Encapsulation;
  * для работы с переменными из другого класса, изменять сами переменные нельзя
  * в классе car есть приватные переменные color и price, которые нужны другим классам, но в этих изменять их нельзя(банковские данные пользователей)
  *
-*/
+ */
 
 public class Chevrolet {
 
-public static void main (String[] args){
+	public static void main (String[] args){
 
-Car car = new Car();
+		Car car = new Car();
 
-System.out.println("Цвет: " + car.getColor());
-System.out.println("Цена: " + car.getPrice());
-}
+		System.out.println("Цвет: " + car.getColor());
+		System.out.println("Цена: " + car.getPrice());
+	}
 }
